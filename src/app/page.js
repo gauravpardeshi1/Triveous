4ceb586ada2b3339cdfb62acb78d13d22ab1c64a
Ga1156 <gauravpardeshi068@gmail.com>
2023-10-01 00:40:56 +0530

    initilize the project by creating Next.js app

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col  bg-white overflow-hidden">
+
+      
+
+
+    </main>
+  )
+}

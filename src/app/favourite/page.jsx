e5d497ee7ed0aaae46c9cfbf96834d6b6ac9aed4
Ga1156 <gauravpardeshi068@gmail.com>
2023-10-02 16:41:30 +0530

    done

--- src/app/favourite/page.jsx
+++ src/app/favourite/page.jsx
@@ -5,7 +5,6 @@ import React, { useEffect, useState } from 'react'
 import ReadMoreButton from '../pages/news/ReadMoreButton'
 import { Toaster, toast } from 'react-hot-toast'
 import { UserAuth } from "../context/AuthContext";
-import PrivateRoute from '../routes/page'
 
 const Favourite = () => {
   const { user, authData } = UserAuth();
